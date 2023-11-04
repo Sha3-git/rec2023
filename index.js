a507e5ff94857b16b4a425159c0e481ee7749e42
Shema Dabiri <dabirishema@gmail.com>
2023-11-04 09:24:08 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const session = require('express-session');
+const fs = require('fs');
+const cors = require('cors');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(cors());
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+const port = 3000;
+app.listen (port, () => {
+    //template literal
+   console.log (`Server is running on http://localhost:${port}`);
+});
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/src/index.html');
+});
